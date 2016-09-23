b6bfe826d418b3024838a18832243b39d8f91754
Joshua Saxby <saxbophone@users.noreply.github.com>
2016-09-23 15:35:26 +0100

    Add MPL Exhibit A Copyright notices to source files

--- leap-arduino.js
+++ leap-arduino.js
@@ -1,3 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2014, Decoded Ltd dev@decoded.com
+ */
+
 // grab the leap data
 var webSocket = require('ws'),
     ws = new webSocket('ws://127.0.0.1:6437'),
--- six-servos.js
+++ six-servos.js
@@ -1,3 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2014, Decoded Ltd dev@decoded.com
+ */
+
 // Map six servos to two hands, trip axis for each hand
 
 // Define acceptable ranges, see leap-range.js
